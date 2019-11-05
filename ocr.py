5d9ea6ff96dc3cac1e7d95783b37c6f9276d8d9f
munjung <jekang0123@gmail.com>
2019-11-06 04:19:24 +0900

    ocr 모듈 완성

--- ocr.py
+++ ocr.py
@@ -5,20 +5,33 @@ import cv2
 import requests
 import sys
 from PIL import Image
+import cgi
+import cgitb
+import urllib
+import ssl
+from threading import Thread
 
 LIMIT_PX = 1024
 LIMIT_BYTE = 1024*1024  # 1MB
 LIMIT_BOX = 40
 
+def image_url_save(image_url: str, image_type: str):
+    ssl._create_default_https_context = ssl._create_unverified_context
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-Agent',
+                          'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(image_url, "img/temp." + image_type)
 
-def kakao_ocr_resize(image_path: str):
+
+def ocr_resize(image_path: str):
 
     image = cv2.imread(image_path)
     volum = os.path.getsize(image_path)
 
     height, width, _ = image.shape
 
-    # check image file size
+    # 용량이 1MB를 초과한다면
     if volum > LIMIT_BYTE:
         im = Image.open(image_path, 'r')
         image_path = "{}_vol.jpg".format(image_path)
@@ -31,24 +44,22 @@ def kakao_ocr_resize(image_path: str):
             image = cv2.resize(image, None, fx=ratio, fy=ratio)
             height, width, _ = height, width, _ = image.shape
             image_path = "{}_resized.jpg".format(image_path)
-            # cv2.imwrite(image_path, image)
             return image_path
-        return image_path
+        else:
+            return image_path
 
     elif LIMIT_PX < height or LIMIT_PX < width:
         ratio = float(LIMIT_PX) / max(height, width)
         image = cv2.resize(image, None, fx=ratio, fy=ratio)
         height, width, _ = height, width, _ = image.shape
-
-        # api 사용전에 이미지가 resize된 경우, recognize시 resize된 결과를 사용해야함.
         image_path = "{}_resized.jpg".format(image_path)
-        # cv2.imwrite(image_path, image)
         return image_path
 
-    return None
+    else:
+        return None
 
 
-def kakao_ocr_detect(image_path: str, appkey: str):
+def ocr_detect(image_path: str, appkey: str):
 
     API_URL = 'https://kapi.kakao.com/v1/vision/text/detect'
     headers = {'Authorization': 'KakaoAK {}'.format(appkey)}
@@ -60,17 +71,8 @@ def kakao_ocr_detect(image_path: str, appkey: str):
     return requests.post(API_URL, headers=headers, files={"file": data})
 
 
-def kakao_ocr_recognize(image_path: str, boxes: list, appkey: str):
-    """
-    recognize api request example
-    :param boxes: 감지된 영역 리스트. Canvas 좌표계: 좌상단이 (0,0) / 우상단이 (limit,0)
-                    감지된 영역중 좌상단 점을 기준으로 시계방향 순서, 좌상->우상->우하->좌하
-                    ex) [[[0,0],[1,0],[1,1],[0,1]], [[1,1],[2,1],[2,2],[1,2]], ...]
-    :param image_path: 이미지 파일 경로
-    :param appkey: 카카오 앱 REST API 키
-    """
+def ocr_recognize(image_path: str, boxes: list, appkey: str):
     API_URL = 'https://kapi.kakao.com/v1/vision/text/recognize'
-
     headers = {'Authorization': 'KakaoAK {}'.format(appkey)}
 
     image = cv2.imread(image_path)
@@ -79,25 +81,38 @@ def kakao_ocr_recognize(image_path: str, boxes: list, appkey: str):
 
     return requests.post(API_URL, headers=headers, files={"file": data}, data={"boxes": json.dumps(boxes)})
 
-
-def main():
-    if len(sys.argv) != 3:
-        print("Please run with args: $ python example.py /path/to/image appkey")
-    image_path, appkey = sys.argv[1], sys.argv[2]
-
-    resize_impath = kakao_ocr_resize(image_path)
-    if resize_impath is not None:
-        image_path = resize_impath
-        print("원본 대신 리사이즈된 이미지를 사용합니다.")
-
-    output = kakao_ocr_detect(image_path, appkey).json()
-    print("[detect] output:\n{}\n".format(output))
-
-    boxes = output["result"]["boxes"]
-    boxes = boxes[:min(len(boxes), LIMIT_BOX)]
-    output = kakao_ocr_recognize(image_path, boxes, appkey).json()
-    print("[recognize] output:\n{}\n".format(json.dumps(output, sort_keys=True, indent=2)))
+def remove_img(image_path):
+    os.remove(image_path)
+    if os.path.exists(image_path) is False:
+        return True
 
 
-if __name__ == "__main__":
-    main()
+cgitb.enable()
+params = cgi.FieldStorage()
+
+image_url = params['img_url'].value
+image_type = params['file_type'].value
+
+t1 = Thread(target=image_url_save, args=(image_url, image_type))
+t1.start()
+t1.join()
+
+image_path = "img/temp."+image_type
+appkey = 'appkey'
+
+resize_impath = ocr_resize(image_path)
+if resize_impath is not None:
+    image_path = resize_impath
+
+output = ocr_detect(image_path, appkey).json()
+boxes = output["result"]["boxes"]
+boxes = boxes[:min(len(boxes), LIMIT_BOX)]
+status = ocr_recognize(image_path, boxes, appkey).status_code
+
+if status == 200:
+    output = ocr_recognize(image_path, boxes, appkey).json()
+    remove_img(image_path)
+    print(output)
+
+else:
+    print(status)
