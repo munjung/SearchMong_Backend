a7f9e28f3c1e323a5daf1d676d5a6b2b39083a93
munjung <jekang0123@gmail.com>
2019-11-09 01:32:08 +0900

    서버 드디어 완성 하....>>>>>>>>>!!ㅓ디ㅏㅓ;ㅣㅏㅓ아ㅣㅣㅣ

--- ocr.py
+++ ocr.py
@@ -1,65 +1,56 @@
-#kakao vision api
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
 
-import json
-import os
-import cv2
-import requests
 import sys
-from PIL import Image
+import codecs
+#sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+
 import cgi
 import cgitb
+cgitb.enable()
+
+print("Content-type: text/html; charset=utf-8\r\n")
+
+import json
+import cv2
+import os
+import requests
+from PIL import Image
 import urllib
+import urllib.request
+import urllib.parse
 import ssl
 from threading import Thread
+from urllib import parse
+import copy
 
 LIMIT_PX = 1024
-LIMIT_BYTE = 1024*1024  # 1MB
+LIMIT_BYTE = 1024*1024
 LIMIT_BOX = 40
 
-def image_url_save(image_url: str, image_type: str):
+def image_url_save(image_url, image_type):
     ssl._create_default_https_context = ssl._create_unverified_context
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-Agent',
                           'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+
     urllib.request.install_opener(opener)
-    urllib.request.urlretrieve(image_url, "temp." + image_type)
+    urllib.request.urlretrieve(image_url, "/home/ubuntu/searchmong/tmp/temp."+image_type)
 
 
 def ocr_resize(image_path: str):
-
     image = cv2.imread(image_path)
-    volum = os.path.getsize(image_path)
-
     height, width, _ = image.shape
 
-    # 용량이 1MB를 초과한다면
-    if volum > LIMIT_BYTE:
-        im = Image.open(image_path, 'r')
-        image_path = "{}_vol.jpg".format(image_path)
-        im.save(image_path, optimize=True, quality=80)
-        image = cv2.imread(image_path)
-        height, width, _ = image.shape
-
-        if LIMIT_PX < height or LIMIT_PX < width:
-            ratio = float(LIMIT_PX) / max(height, width)
-            image = cv2.resize(image, None, fx=ratio, fy=ratio)
-            height, width, _ = height, width, _ = image.shape
-            image_path = "{}_resized.jpg".format(image_path)
-            return image_path
-
-        else:
-            return image_path
-
-    elif LIMIT_PX < height or LIMIT_PX < width:
+    if LIMIT_PX < height or LIMIT_PX < width:
         ratio = float(LIMIT_PX) / max(height, width)
         image = cv2.resize(image, None, fx=ratio, fy=ratio)
         height, width, _ = height, width, _ = image.shape
         image_path = "{}_resized.jpg".format(image_path)
+        cv2.imwrite(image_path, image)
+
         return image_path
-
-    else:
-        return None
-
+    return None
 
 def ocr_detect(image_path: str, appkey: str):
 
@@ -88,19 +79,18 @@ def remove_img(image_path):
     if os.path.exists(image_path) is False:
         return True
 
-
-cgitb.enable()
 params = cgi.FieldStorage()
+image_url = params["image_url"].value
+image_type = params['image_type'].value
 
-image_url = params['img_url'].value
-image_type = params['file_type'].value
+image_url = image_url.replace('"'," ")
+image_type = image_type.replace('"'," ")
 
-t1 = Thread(target=image_url_save, args=(image_url, image_type))
-t1.start()
-t1.join()
+image_url_save(image_url,image_type)
 
-image_path = "temp."+image_type
-appkey = 'appkey'
+image_path = "/home/ubuntu/searchmong/tmp/temp."+image_type
+#print(image_path)
+appkey = "appkey"
 
 resize_impath = ocr_resize(image_path)
 if resize_impath is not None:
@@ -113,8 +103,8 @@ status = ocr_recognize(image_path, boxes, appkey).status_code
 
 if status == 200:
     output = ocr_recognize(image_path, boxes, appkey).json()
-    remove_img(image_path)
-    print(output)
+   # remove_img(image_path)
+    print(output["result"]["recognition_words"])
 
 else:
     print(status)
--- transfer_search.py
+++ transfer_search.py
@@ -1,70 +1,66 @@
-import urllib.request
-import ssl
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import sys
+import codecs
 import cgi
 import cgitb
-import json
-from urllib.parse import quote
-from threading import Thread
-
 cgitb.enable()
+print ("Content-type: text/html; charset=utf-8\r\n")
+import json
+import urllib.request
+import ssl
+from urllib.parse import quote
 
-appkey = 'appkey'
+appkey = 'key'
 client_id = 'id'
 client_secret = 'sec'
 
 status = 0
 kor_res = ''
 
-def eng_to_kor(keyword: str):
-    encText = urllib.parse.quote(keyword)
-    data = "source=en&target=ko&text=" + encText
-    url = "https://openapi.naver.com/v1/language/translate"
 
+def eng_to_kor(keyword: str):
+    url = "https://openapi.naver.com/v1/language/translate"
+    data = 'source=en&target=ko&text='+keyword
     request = urllib.request.Request(url)
-    request.add_header("X-Naver-Client-Id", client_id)
-    request.add_header("X-Naver-Client-Secret", client_secret)
+    request.add_header('X-Naver-Client-Id', client_id)
+    request.add_header('X-Naver-Client-Secret', client_secret)
     ssl._create_default_https_context = ssl._create_unverified_context
     response = urllib.request.urlopen(request, data=data.encode("utf-8"))
     rescode = response.getcode()
     if rescode == 200:
-        response_body = response.read().decode('utf-8')
-        json_data = json.loads(response_body)
-        global status
-        global kor_res
-        status = 1
-        kor_res = json_data["message"]["result"]["translatedText"]
+        response_body = response.read()
+        print(response_body)
         return kor_res
 
     else:
         return "Error"
 
 
+params = cgi.FieldStorage()
+keyword = params['keyword'].value
+find_image = params['find_image'].value
+tmp = params['tmp'].value
 
-def find_image(keyword_str: str):
-    keyword_str = quote(keyword_str)
+#print(int(tmp))
+
+if int(tmp) == 0: #papago
+    keyword = keyword.replace('"',"")
+    res = eng_to_kor(keyword)
+    print(res)
+
+else: #image research
+    find_image = find_image.replace('"',"")
+    enc = urllib.parse.quote(find_image)
     url = 'https://dapi.kakao.com/v2/search/image'
     request = urllib.request.Request(url)
     request.add_header('Authorization', 'KakaoAK {}'.format(appkey))
-    data = 'query=' + keyword_str + "&sort=accuracy"
+    data = 'query=' + enc + "&sort=accuracy"
     ssl._create_default_https_context = ssl._create_unverified_context
     response = urllib.request.urlopen(request, data=data.encode("utf-8"))
     rescode = response.getcode()
     if rescode == 200:
-        res = response.read().decode('utf-8')
-        print(res)
+        print(response.read())
     else:
         print("Error Code:" + rescode)
-
-
-params = cgi.FieldStorage()
-keyword = params['keyword'].value
-
-t1 = Thread(target=eng_to_kor, args=keyword)
-t1.start()
-t1.join()
-
-if status > 0:
-    find_image(kor_res)
-
-else:
-    print("Error")
